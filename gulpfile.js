a867211e94c1b9dc63e5655bfd4a8006dd5179ee
chenyumiao <chenyumiao@jd.com>
2017-02-07 16:31:13 +0800

    gulp修改

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,9 @@
  * @Email 【chenyumiao@jd.com】
  * @Date 2017/2/7 11:00
  */
-var gulp = require('gulp'),
+var gulp = require('gulp');
+
+    gulp.task('default', function() {
+    // 将你的默认的任务代码放在这
+   });
    
